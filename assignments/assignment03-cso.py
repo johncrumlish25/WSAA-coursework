b190593cfbd784272ddf995c4ad642d31735870a
johncrumlish25 <johncrumlish16@gmail.com>
2026-02-17 18:15:51 +0000

    Added some code to assignment03

--- assignments/assignment03-cso.py
+++ assignments/assignment03-cso.py
@@ -2,3 +2,10 @@
 # This program will retrieve the dataset for the "exchequer account (historical series)" from the CSO
 # Author: John Crumlish
 
+import request
+import json
+
+url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
+
+response = request.get(url)
+return response.json()
