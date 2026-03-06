d1cbed4a50d4fe12d650f5cebf6b9a6133c439a4
johncrumlish25 <johncrumlish16@gmail.com>
2026-03-06 13:07:57 +0000

    Added files to python-database-lab

---
+++ labs/python-databases-lab/mysql_update_data.py
@@ -0,0 +1,25 @@
+# mysql_update data.py
+# Update data
+# Author: John Crumlish
+
+import mysql.connector 
+ 
+db = mysql.connector.connect( 
+  host="localhost", 
+  user="root", 
+  password="", 
+  #user="datarep",  # this is the user name on my mac 
+  #passwd="password" # for my mac 
+  database="wsaa" 
+) 
+ 
+cursor = db.cursor() 
+sql="update student set name= %s, age=%s  where id = %s" 
+values = ("Joe",33, 1) 
+ 
+cursor.execute(sql, values) 
+ 
+db.commit() 
+print("update done") 
+mycursor.close() 
+connection.close() 
