66a5249561c47f8dafe8d6a312a36c6be6f24788
johncrumlish25 <johncrumlish16@gmail.com>
2026-04-10 13:03:32 +0100

    Improved the comments in assignment02

--- assignments/assignment02-carddraw.py
+++ assignments/assignment02-carddraw.py
@@ -1,19 +1,19 @@
-# assignment2-carddraw.py
+# assignment02-carddraw.py
 # API that simulates dealing a deck of cards
 # Author: John Crumlish
 
 import requests
 
-# Shuffle Deck
+# Request a new shuffled deck from the Deck of Cards API
 shuffle = requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1").json()
 deck_id = shuffle["deck_id"]
 
-# Draw 5 Cards
+# Draw 5 cards from the shuffled deck using the deck ID
 draw = requests.get(f"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count=5").json()
 cards = draw["cards"]
 
 print("Your cards:\n")
 
-# Print Each Card
+# Print each cards suit and value
 for card in cards:
     print(card["value"], "of", card["suit"])
